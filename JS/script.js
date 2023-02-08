860cd0b8a21f3310baffe2937c9945ddca27912c
Yuliia <example@gmail.com>
2023-02-08 19:56:08 +0100

    1task

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,20 @@
-console.log(1);
+"use strict"
+let numderOfFilms = +promt ("Сколько фильмов вы уже посмотрели?", "");
+
+const personalMovieDB {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+let a = promt ( "Один из послeдниx просмотренных фильмов?", "",);
+    b = promt ("На сколько оцените его", "");
+    c = promt ( "Один из послeдниx просмотренных фильмов?", "",);
+    d = promt ("На сколько оцените его", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
